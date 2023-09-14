5dc2e5d0a80521b5785749dc3f5061d25052415f
Gabriel Silva <63270700+gabfsilva23@users.noreply.github.com>
2023-09-14 00:11:57 -0300

    Add files via upload

---
+++ Integer/src/Integer.java
@@ -0,0 +1,15 @@
+public class Integer {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 2;
+		int c = 0;
+		int d = 9999999;
+		int e = -700;
+
+		System.out.println( a * b);
+		System.out.println( d - e);
+	}
+
+}
