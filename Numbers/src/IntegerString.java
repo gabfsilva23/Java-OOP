0efbc1df70f7a621ea1b9f61bdf0eeab88bb8acf
Gabriel Silva <63270700+gabfsilva23@users.noreply.github.com>
2023-09-18 02:49:59 -0300

    Add files via upload

---
+++ Numbers/src/IntegerString.java
@@ -0,0 +1,14 @@
+public class IntegerString {
+
+	public static void main(String[] args) {
+
+		String s = "age: 47";
+		s = s.replaceAll("\\D+","");
+		
+		int n = Integer.parseInt(s);
+		
+		System.out.println(n + 2);
+		
+	}
+
+}
