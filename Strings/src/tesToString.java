07bfce01b08bacf2d1bbb31f8ebdc59e664d15f0
Gabriel Silva <63270700+gabfsilva23@users.noreply.github.com>
2023-09-18 02:28:39 -0300

    Add files via upload

---
+++ Strings/src/tesToString.java
@@ -0,0 +1,19 @@
+public class tesToString {
+    private int id;
+    private String name;
+
+    public tesToString(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "tesToString{id=" + id + ", name='" + name + "'}";
+    }
+
+    public static void main(String[] args) {
+    	tesToString obj = new tesToString(1, "NOME");
+        System.out.println(obj.toString()); // Calls the overridden toString() method
+    }
+}
